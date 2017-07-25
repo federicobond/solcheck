5fff12cdeb935e295c3e5675367dcc05debda9cf
Federico Bond <federicobond@gmail.com>
2017-07-24 22:04:31 -0300

    Initial commit

---
+++ src/rules/deprecated-suicide.js
@@ -0,0 +1,25 @@
+module.exports = {
+    meta: {
+        docs: {
+            description: "Replace deprecated 'suicide' with 'selfdestruct'",
+            category: "Best Practices",
+        },
+    },
+
+    create(context) {
+        function isSuicide(node) {
+            return node.expression.type == 'Identifier' && node.expression.name == 'suicide'
+        }
+
+        return {
+            FunctionCall(node) {
+                if (isSuicide(node)) {
+                    context.report({
+                        node: node,
+                        message: "Replace deprecated 'suicide' with 'selfdestruct'.",
+                    });
+                }
+            }
+        };
+    }
+};
---
+++ test/test.sol
@@ -0,0 +1,5 @@
+contract test {
+    function() payable {
+        suicide(0x0);
+    }
+}
