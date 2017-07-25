54eb1ed3510d34ea05431547b739f3f0db45104e
Federico Bond <federicobond@gmail.com>
2017-07-24 22:12:05 -0300

    Run prettier

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import findUp from 'find-up'
-import fs from 'fs'
-import glob from 'glob'
-import parser from 'solidity-parser-antlr'
+import findUp from "find-up"
+import fs from "fs"
+import glob from "glob"
+import parser from "solidity-parser-antlr"
 
-const formatter = require('eslint/lib/formatters/codeframe')
+const formatter = require("eslint/lib/formatters/codeframe")
 
-const NAME = 'solcheck'
+const NAME = "solcheck"
 
-function lint (files, argv) {
+function lint(files, argv) {
   let results = []
 
   glob(files[0], (err, matches) => {
     for (let filePath of matches) {
-      const source = fs.readFileSync(filePath).toString('utf-8')
+      const source = fs.readFileSync(filePath).toString("utf-8")
       const ast = parser.parse(source, { loc: true, range: true })
       const report = processFile(filePath, source, ast)
       results.push(report)
@@ -22,17 +22,17 @@ function lint (files, argv) {
   })
 }
 
-function getRules () {
+function getRules() {
   return [
     {
-      ruleId: 'deprecated-suicide',
-      rule: require('./rules/deprecated-suicide'),
+      ruleId: "deprecated-suicide",
+      rule: require("./rules/deprecated-suicide"),
       severity: 2
     }
   ]
 }
 
-function processFile (filePath, source, ast) {
+function processFile(filePath, source, ast) {
   let messages = []
 
   let errorCount = 1
@@ -41,17 +41,20 @@ function processFile (filePath, source, ast) {
   for (let { ruleId, rule, severity } of getRules()) {
     let context = {
       report(obj) {
-          let node = obj.node
-          delete obj.node
+        let node = obj.node
+        delete obj.node
 
-          const message = Object.assign({
+        const message = Object.assign(
+          {
             ruleId,
             line: node.loc.start.line,
             column: node.loc.start.column + 1,
             severity: 2
-          }, obj)
+          },
+          obj
+        )
 
-          messages.push(message)
+        messages.push(message)
       }
     }
     parser.visit(ast, rule.create(context))
@@ -60,20 +63,19 @@ function processFile (filePath, source, ast) {
   return Object({ errorCount, warningCount, filePath, source, messages })
 }
 
-module.exports = function main () {
+module.exports = function main() {
   const configPath = findUp.sync([`.${NAME}rc`])
   const config = configPath ? JSON.parse(fs.readFileSync(configPath)) : {}
 
-  const argv = require('yargs')
-    .usage('Usage: $0 [options] <files>')
-    .option('verbose', {
-      alias: 'v',
+  const argv = require("yargs")
+    .usage("Usage: $0 [options] <files>")
+    .option("verbose", {
+      alias: "v",
       default: false
     })
     .help()
     .config(config)
-    .pkgConf(NAME)
-    .argv
+    .pkgConf(NAME).argv
 
   const files = argv._
 
--- src/rules/deprecated-suicide.js
+++ src/rules/deprecated-suicide.js
@@ -1,25 +1,28 @@
 module.exports = {
-    meta: {
-        docs: {
-            description: "Replace deprecated 'suicide' with 'selfdestruct'",
-            category: "Best Practices",
-        },
-    },
-
-    create(context) {
-        function isSuicide(node) {
-            return node.expression.type == 'Identifier' && node.expression.name == 'suicide'
-        }
-
-        return {
-            FunctionCall(node) {
-                if (isSuicide(node)) {
-                    context.report({
-                        node: node,
-                        message: "Replace deprecated 'suicide' with 'selfdestruct'.",
-                    });
-                }
-            }
-        };
+  meta: {
+    docs: {
+      description: "Replace deprecated 'suicide' with 'selfdestruct'",
+      category: "Best Practices"
     }
-};
+  },
+
+  create(context) {
+    function isSuicide(node) {
+      return (
+        node.expression.type == "Identifier" &&
+        node.expression.name == "suicide"
+      )
+    }
+
+    return {
+      FunctionCall(node) {
+        if (isSuicide(node)) {
+          context.report({
+            node: node,
+            message: "Replace deprecated 'suicide' with 'selfdestruct'."
+          })
+        }
+      }
+    }
+  }
+}
